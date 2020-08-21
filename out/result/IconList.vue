2852c24aef09d00325afefb5152aa7daf35a2e72
Ivan Shavliuga <ivanov-R0M@yandex.ru>
2020-08-21 09:41:54 +0300

    version 0.2.0

---
+++ out/result/IconList.vue
@@ -0,0 +1,165 @@
+<template>
+  <section>
+<icon-base class="icons__image" icon-name="Icon-grinning" width="64" height="64" icon-color="#5434ad"><Icon-grinning/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-smiling" width="64" height="64" icon-color="#5434ad"><Icon-smiling/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-tongue" width="64" height="64" icon-color="#5434ad"><Icon-tongue/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-unamused" width="64" height="64" icon-color="#5434ad"><Icon-unamused/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-nerd" width="64" height="64" icon-color="#5434ad"><Icon-nerd/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-drooling" width="64" height="64" icon-color="#5434ad"><Icon-drooling/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-kiss" width="64" height="64" icon-color="#5434ad"><Icon-kiss/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-mouthless" width="64" height="64" icon-color="#5434ad"><Icon-mouthless/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-joy" width="64" height="64" icon-color="#5434ad"><Icon-joy/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-halo" width="64" height="64" icon-color="#5434ad"><Icon-halo/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-winking" width="64" height="64" icon-color="#5434ad"><Icon-winking/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-smirking" width="64" height="64" icon-color="#5434ad"><Icon-smirking/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-sleeping" width="64" height="64" icon-color="#5434ad"><Icon-sleeping/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-woozy" width="64" height="64" icon-color="#5434ad"><Icon-woozy/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-astonished" width="64" height="64" icon-color="#5434ad"><Icon-astonished/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-upside down" width="64" height="64" icon-color="#5434ad"><Icon-upside down/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-horns" width="64" height="64" icon-color="#5434ad"><Icon-horns/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-vomiting" width="64" height="64" icon-color="#5434ad"><Icon-vomiting/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-grinning" width="64" height="64" icon-color="#5434ad"><Icon-grinning/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-winking" width="64" height="64" icon-color="#5434ad"><Icon-winking/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-kissing" width="64" height="64" icon-color="#5434ad"><Icon-kissing/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-rolling" width="64" height="64" icon-color="#5434ad"><Icon-rolling/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-no expression" width="64" height="64" icon-color="#5434ad"><Icon-no expression/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-flushed" width="64" height="64" icon-color="#5434ad"><Icon-flushed/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-emojis" width="64" height="64" icon-color="#5434ad"><Icon-emojis/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-angry" width="64" height="64" icon-color="#5434ad"><Icon-angry/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-grimacing" width="64" height="64" icon-color="#5434ad"><Icon-grimacing/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-sleepy" width="64" height="64" icon-color="#5434ad"><Icon-sleepy/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-squit" width="64" height="64" icon-color="#5434ad"><Icon-squit/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-hearts" width="64" height="64" icon-color="#5434ad"><Icon-hearts/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-worry" width="64" height="64" icon-color="#5434ad"><Icon-worry/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-in love" width="64" height="64" icon-color="#5434ad"><Icon-in love/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-moustache" width="64" height="64" icon-color="#5434ad"><Icon-moustache/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-crying" width="64" height="64" icon-color="#5434ad"><Icon-crying/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-sad" width="64" height="64" icon-color="#5434ad"><Icon-sad/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-pirate" width="64" height="64" icon-color="#5434ad"><Icon-pirate/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-sad" width="64" height="64" icon-color="#5434ad"><Icon-sad/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-cowardly" width="64" height="64" icon-color="#5434ad"><Icon-cowardly/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-monocle" width="64" height="64" icon-color="#5434ad"><Icon-monocle/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-smile" width="64" height="64" icon-color="#5434ad"><Icon-smile/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-angry" width="64" height="64" icon-color="#5434ad"><Icon-angry/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-muted" width="64" height="64" icon-color="#5434ad"><Icon-muted/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-pensive" width="64" height="64" icon-color="#5434ad"><Icon-pensive/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-displeased" width="64" height="64" icon-color="#5434ad"><Icon-displeased/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-party" width="64" height="64" icon-color="#5434ad"><Icon-party/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-neutral" width="64" height="64" icon-color="#5434ad"><Icon-neutral/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-stars" width="64" height="64" icon-color="#5434ad"><Icon-stars/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-nausea" width="64" height="64" icon-color="#5434ad"><Icon-nausea/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-dead" width="64" height="64" icon-color="#5434ad"><Icon-dead/></icon-base>"
+<icon-base class="icons__image" icon-name="Icon-tired" width="64" height="64" icon-color="#5434ad"><Icon-tired/></icon-base>"
+  </section>
+</template>
+<script>
+import IconBase from './IconBase'
+
+import Icon-grinning from 'vue/Icon-grinning.vue'
+import Icon-smiling from 'vue/Icon-smiling.vue'
+import Icon-tongue from 'vue/Icon-tongue.vue'
+import Icon-unamused from 'vue/Icon-unamused.vue'
+import Icon-nerd from 'vue/Icon-nerd.vue'
+import Icon-drooling from 'vue/Icon-drooling.vue'
+import Icon-kiss from 'vue/Icon-kiss.vue'
+import Icon-mouthless from 'vue/Icon-mouthless.vue'
+import Icon-joy from 'vue/Icon-joy.vue'
+import Icon-halo from 'vue/Icon-halo.vue'
+import Icon-winking from 'vue/Icon-winking.vue'
+import Icon-smirking from 'vue/Icon-smirking.vue'
+import Icon-sleeping from 'vue/Icon-sleeping.vue'
+import Icon-woozy from 'vue/Icon-woozy.vue'
+import Icon-astonished from 'vue/Icon-astonished.vue'
+import Icon-upside down from 'vue/Icon-upside down.vue'
+import Icon-horns from 'vue/Icon-horns.vue'
+import Icon-vomiting from 'vue/Icon-vomiting.vue'
+import Icon-grinning from 'vue/Icon-grinning.vue'
+import Icon-winking from 'vue/Icon-winking.vue'
+import Icon-kissing from 'vue/Icon-kissing.vue'
+import Icon-rolling from 'vue/Icon-rolling.vue'
+import Icon-no expression from 'vue/Icon-no expression.vue'
+import Icon-flushed from 'vue/Icon-flushed.vue'
+import Icon-emojis from 'vue/Icon-emojis.vue'
+import Icon-angry from 'vue/Icon-angry.vue'
+import Icon-grimacing from 'vue/Icon-grimacing.vue'
+import Icon-sleepy from 'vue/Icon-sleepy.vue'
+import Icon-squit from 'vue/Icon-squit.vue'
+import Icon-hearts from 'vue/Icon-hearts.vue'
+import Icon-worry from 'vue/Icon-worry.vue'
+import Icon-in love from 'vue/Icon-in love.vue'
+import Icon-moustache from 'vue/Icon-moustache.vue'
+import Icon-crying from 'vue/Icon-crying.vue'
+import Icon-sad from 'vue/Icon-sad.vue'
+import Icon-pirate from 'vue/Icon-pirate.vue'
+import Icon-sad from 'vue/Icon-sad.vue'
+import Icon-cowardly from 'vue/Icon-cowardly.vue'
+import Icon-monocle from 'vue/Icon-monocle.vue'
+import Icon-smile from 'vue/Icon-smile.vue'
+import Icon-angry from 'vue/Icon-angry.vue'
+import Icon-muted from 'vue/Icon-muted.vue'
+import Icon-pensive from 'vue/Icon-pensive.vue'
+import Icon-displeased from 'vue/Icon-displeased.vue'
+import Icon-party from 'vue/Icon-party.vue'
+import Icon-neutral from 'vue/Icon-neutral.vue'
+import Icon-stars from 'vue/Icon-stars.vue'
+import Icon-nausea from 'vue/Icon-nausea.vue'
+import Icon-dead from 'vue/Icon-dead.vue'
+import Icon-tired from 'vue/Icon-tired.vue'
+export default {
+  components: {
+    
+
+Icon-grinning,
+Icon-smiling,
+Icon-tongue,
+Icon-unamused,
+Icon-nerd,
+Icon-drooling,
+Icon-kiss,
+Icon-mouthless,
+Icon-joy,
+Icon-halo,
+Icon-winking,
+Icon-smirking,
+Icon-sleeping,
+Icon-woozy,
+Icon-astonished,
+Icon-upside down,
+Icon-horns,
+Icon-vomiting,
+Icon-grinning,
+Icon-winking,
+Icon-kissing,
+Icon-rolling,
+Icon-no expression,
+Icon-flushed,
+Icon-emojis,
+Icon-angry,
+Icon-grimacing,
+Icon-sleepy,
+Icon-squit,
+Icon-hearts,
+Icon-worry,
+Icon-in love,
+Icon-moustache,
+Icon-crying,
+Icon-sad,
+Icon-pirate,
+Icon-sad,
+Icon-cowardly,
+Icon-monocle,
+Icon-smile,
+Icon-angry,
+Icon-muted,
+Icon-pensive,
+Icon-displeased,
+Icon-party,
+Icon-neutral,
+Icon-stars,
+Icon-nausea,
+Icon-dead,
+Icon-tired,
+    }
+}
+
+</script>
